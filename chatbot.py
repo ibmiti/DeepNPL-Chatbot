6d35c6584c8eb585ba77655326e6c6813428b270
oliver olive <oo@olivers-MBP.attlocal.net>
2020-10-03 12:57:47 -0400

    adding method for cleaning text

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@
 # importing libraries 
 import numpy as np
 import tensorflow as tf 
-import re as r
+import re as replace
 import time as t
 
 # -----------------------------Gaining access to data, as GAD  ---------------
@@ -36,4 +36,22 @@ for conversation in conversations_ids:
         questions.append(id2line[conversation[i]])
         answers.append(id2line[conversation[i+1]])
             
-
+# Doing a first cleaning of the texts        
+# the argument for this param will be either questions or answers         
+def clean_text(text):
+    text = text.lower()
+    text = replace.sub(r"i'm", "i am", text)
+    text = replace.sub(r"he's", "he is", text)
+    text = replace.sub(r"she's", "she is", text)
+    text = replace.sub(r"that's", "that is", text)
+    text = replace.sub(r"what's", "what is", text)
+    text = replace.sub(r"where's", "where is", text)
+    text = replace.sub(r"\'ll", " will", text)
+    text = replace.sub(r"\'ve", " have", text)
+    text = replace.sub(r"\'re", " are", text)
+    text = replace.sub(r"\'d", " would", text)
+    text = replace.sub(r"won't", "will not", text)    
+    text = replace.sub(r"can't", "cannot", text)
+    text = replace.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
+    return text
+    
