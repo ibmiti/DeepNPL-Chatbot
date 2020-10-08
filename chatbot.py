01b560c893d98f5767eb096ccd47f59b27af3b76
oliver olive <oo@olivers-MBP.attlocal.net>
2020-10-08 06:40:57 -0400

    adding the decoder_rnn layer

--- chatbot.py
+++ chatbot.py
@@ -197,7 +197,7 @@ def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, seq
                                                                               attention_values,
                                                                               attention_score_function,
                                                                               attention_construct_function,
-                                                                              name = "attn_dec_train")
+                                                                              name = "attn_dec_train") # training mode
     decoder_output, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
                                                                                                               training_decoder_function,
                                                                                                               decoder_embedded_input,
@@ -207,8 +207,69 @@ def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, seq
     decoder_output_dropout = tf.nn.dropout(decoder_output, keep_prob)
     return output_function(decoder_output_dropout)
 
+# Decoding the test/validation set
+def decode_test_set(encoder_state, decoder_cell, decoder_embeddings_matrix, sos_id, eos_id, maximum_length, num_words, sequence_length, decoding_scope, output_function, keep_prob, batch_size):
+    attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option = 'bahdanau', num_units = decoder_cell.output_size)
+    test_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_inference(output_function,
+                                                                              encoder_state[0],
+                                                                              attention_keys,
+                                                                              attention_values,
+                                                                              attention_score_function,
+                                                                              attention_construct_function,
+                                                                              decoder_embeddings_matrix,
+                                                                              sos_id,
+                                                                              eos_id,
+                                                                              maximum_length,
+                                                                              num_words,
+                                                                              name = "attn_dec_inf") # inference mode
+    test_predictions, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell,
+                                                                                                                training_decoder_function,
+                                                                                                                scope = decoding_scope)
+    return test_predictions
 
-    
+# creating decoder rnn layer
+def decoder_rnn(decoder_embedded_inputs,decoder_embedding_matrix,encoder_state,num_words,sequence_length,rnn_size,num_layers, word2int,keep_prob, batch_size):
+    # importing into function scope tf.var...
+    with tf.variable_scope("decoding") as decoding_scope:
+        lstm         = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+        lstm_dropout = tf.contrib.DropoutWrapper(lstm,input_keep_prob = keep_prob)
+        # creates stacked multi rnn layer 
+        decoder_cell = tf.contrib.MultiRNNCell([lstm_dropout] * num_layers)
+        weights      = tf.truncated_normal_initializer(stddev = 0.1)
+        biases       = tf.zeros_initializer()
+        ouput_function = lambda x: tf.contrib.layers.fully_connected(x,
+                                                                     num_words,
+                                                                     None,
+                                                                     scope = decoding_scope,
+                                                                     weights_intializer = weights,
+                                                                     biases_initializer = biases)
+        # predictions 
+        training_predictions = decode_training_set(encoder_state,
+                                                 decoder_cell,
+                                                 decoder_embedded_input,
+                                                 sequence_length,
+                                                 decoding_scope,
+                                                 output_function,
+                                                 keep_prob,
+                                                 batch_size)
+        
+        ## make sure there are no typos when coming back to this
+        decoding_scope.reuse_variables()
+        test_predictions = decode_test_set(encoder_state,
+                                           decoder_cell,
+                                           decoder_embeddings_matrix,
+                                           word2int['<SOS>'],
+                                           word2int['<EOS>'],
+                                           sequence_length - 1,
+                                           num_words,
+                                           deocding_scope,
+                                           output_function,
+                                           keep_prob,
+                                           batch_size)
+    return training_predictions, test_predictions
+## make sure to pseudo code the above when done ##
+
     
     
     
